7e4f2a96efe9ee9a6a1770610dfeaf2cd13491b8
Rabab-art <bouhamla.rabab@gmail.com>
2021-10-20 20:06:24 +0200

    Inseriemnto di file vue

---
+++ resources/js/components/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <din class="row justify-content-center">
+           <div class="col-md-8">
+               <div class="card">
+                   <Header :title="title"/>
+                   <div class="card-body">
+                       Sito in costruzione
+                   </div>
+               </div>
+           </div>
+        </din>
+    </div>
+</template>
+
+<script>
+import Header from "./Header.vue";
+
+export default {
+    name:'App',
+    data() {
+      return { title:"Il mio Blog!"}  ;
+    },
+    componets:{
+        Header,
+    },
+};
+</script>
